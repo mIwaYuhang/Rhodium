6a1903f5be966c12636d6c8750d94e3c537c3204
Twelve <bionicvnb@gmail.com>
2023-11-03 03:20:29 +0700

    Anti-scam stuffs

--- src/main/java/emu/rhodium/Rhodium.java
+++ src/main/java/emu/rhodium/Rhodium.java
@@ -2,6 +2,6 @@ package emu.rhodium;
 
 public class Rhodium {
     public static void main(String[] args) throws Exception {
-
+        System.out.println("This project is free and open source. If you purchased this, you have been scammed.");
     }
 }
