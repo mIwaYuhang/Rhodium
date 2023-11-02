144606e3d7af06632c25dc8d3464cc648b589de9
Twelve <bionicvnb@gmail.com>
2023-11-03 03:09:03 +0700

    Creating dummy config file.

--- src/main/java/emu/rhodium/Rhodium.java
+++ src/main/java/emu/rhodium/Rhodium.java
@@ -1,9 +1,5 @@
 package emu.rhodium;
 
-import emu.rhodium.server.ServerCore;
-import emu.rhodium.config.Configuration;
-import emu.rhodium.game.GameManager;
-
 public class Rhodium {
     public static void main(String[] args) throws Exception {
 
--- src/main/java/emu/rhodium/config/Configuration.java
+++
@@ -1,7 +0,0 @@
-package emu.rhodium.config;
-
-public class Configuration extends ConfigContainer {
-    public static void OverrideMe() {
-        System.out.println("Overridden.");
-    }
-}
