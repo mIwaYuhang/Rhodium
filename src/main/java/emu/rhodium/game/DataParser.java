84e2ce5a5459cb39688a3673e7259c7f98b09f67
Twelve <bionicvnb@gmail.com>
2023-11-19 17:47:40 +0700

    Beta 1.1

--- src/main/java/emu/rhodium/game/DataParser.java
+++ src/main/java/emu/rhodium/game/DataParser.java
@@ -1,8 +1,34 @@
 package emu.rhodium.game;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
 
 public class DataParser {
+    public static JsonObject GetData(String Path) {
+        Gson Parser = new Gson();
+        File JsonFile = new File(Path);
+        BufferedReader BufRead = null;
+        JsonObject ParsedFile;
+
+        try {
+            BufRead = new BufferedReader(new FileReader(JsonFile));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            ParsedFile = Parser.fromJson(BufRead, JsonObject.class);
+        } catch (JsonSyntaxException e) {
+            throw e;
+        }
+
+        return ParsedFile;
+    }
+
+
 }
