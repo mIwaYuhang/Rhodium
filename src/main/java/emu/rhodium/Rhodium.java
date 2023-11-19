e63178d93d3faaa3d65855ebe375a70b8f4f31c5
Twelve <bionicvnb@gmail.com>
2023-11-19 17:55:51 +0700

    Beta 1.2

--- src/main/java/emu/rhodium/Rhodium.java
+++ src/main/java/emu/rhodium/Rhodium.java
@@ -1,7 +1,11 @@
 package emu.rhodium;
 
+import emu.rhodium.game.DataParser;
+
 public class Rhodium {
     public static void main(String[] args) throws Exception {
         System.out.println("This project is free and open source. If you purchased this, you have been scammed.");
+
+        System.out.println(DataParser.GetActivityTable());
     }
 }
